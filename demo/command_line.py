ba0e7e829ff244077e39dc9b1f5768eef96fdfd9
fayewang <fayewang@tencent.com>
2021-01-13 11:26:12 +0800

    区分错误码

--- demo/command_line.py
+++ demo/command_line.py
@@ -5,6 +5,59 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import python_atom_sdk as sdk
+from .error_code import ErrorCode
+
+
+err_code = ErrorCode()
+
+
+def exit_with_error(error_type=None, error_code=None, error_msg="failed"):
+    """
+    @summary: exit with error
+    """
+    if not error_type:
+        error_type = sdk.OutputErrorType.PLUGIN
+    if not error_code:
+        error_code = err_code.PLUGIN_ERROR
+    sdk.log.error("error_type: {}, error_code: {}, error_msg: {}".format(error_type, error_code, error_msg))
+
+    output_data = {
+        "status":    sdk.status.FAILURE,
+        "errorType": error_type,
+        "errorCode": error_code,
+        "message":   error_msg,
+        "type":      sdk.output_template_type.DEFAULT
+    }
+    sdk.set_output(output_data)
+
+    exit(error_code)
+
+
+def exit_with_succ(data=None, quality_data=None, msg="run succ"):
+    """
+    @summary: exit with succ
+    """
+    if not data:
+        data = {}
+
+    output_template = sdk.output_template_type.DEFAULT
+    if quality_data:
+        output_template = sdk.output_template_type.QUALITY
+
+    output_data = {
+        "status":  sdk.status.SUCCESS,
+        "message": msg,
+        "type":    output_template,
+        "data":    data
+    }
+
+    if quality_data:
+        output_data["qualityData"] = quality_data
+
+    sdk.set_output(output_data)
+
+    sdk.log.info("finish")
+    exit(err_code.OK)
 
 
 def main():
@@ -15,25 +68,25 @@ def main():
 
     # 输入
     input_params = sdk.get_input()
-    sdk.log.info(input_params)
+
+    # 获取名为input_demo的输入字段值
+    input_demo = input_params.get("input_demo", None)
+    sdk.log.info("input_demo is {}".format(input_demo))
+    if not input_demo:
+        exit_with_error(error_type=sdk.output_error_type.USER,
+                        error_code=err_code.USER_CONFIG_ERROR,
+                        error_msg="input_demo is None")
 
     # 插件逻辑
-    sdk.log.info("hello")
+    sdk.log.info("Hello world!")
 
     # 插件执行结果、输出数据
-    output_data = {
-        "status": sdk.status.SUCCESS,
-        "message": "run succ",
-        "type": sdk.output_template_type.DEFAULT,
-        "data": {
-            "code_git_mr_review_status": {
-                "type": sdk.output_field_type.STRING,
-                "value": "test output"
-            }
+    data = {
+        "output_demo": {
+            "type": sdk.output_field_type.STRING,
+            "value": "test output"
         }
     }
-    sdk.set_output(output_data)
-
-    sdk.log.info("finish")
+    exit_with_succ(data=data)
 
     exit(0)
---
+++ demo/error_code.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class ErrorCode(object):
+    OK = 0                       # 无异常
+    PLUGIN_ERROR = 2199001       # 插件自身异常
+    USER_CONFIG_ERROR = 2199002  # 用户配置有误
